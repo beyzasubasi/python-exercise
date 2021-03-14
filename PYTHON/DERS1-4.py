39b0f7224c1865d597300820cd4d8650081df6f5
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:22:01 +0300

    Create DERS1-4.py

---
+++ PYTHON/DERS1-4.py
@@ -0,0 +1,19 @@
+#katsayıları bilinen 2.dereceden bir bilinmeyenli kökleri bulan algo
+
+a = int(input("x karenin katsayısını giriniz: "))
+b = int(input("x in katsayısını giriniz: "))
+c = int(input("sabit terimi giriniz: "))
+
+delta = b**2-4*a*c
+
+if(delta>0):
+    x1 = (-b + delta**(1/2)) / (2*a)
+    x2 = (-b - delta**(1/2)) / (2*a)
+    print(x1, x2)
+
+elif(delta==0):       # = : atama işlemidir hata verdi
+    x1 = -b/(2*a)     # == : eşit mi demektir bunu kullanmamız gerekiyor
+    print(x1)
+
+else:
+    print("Reel Kök Yoktur!")
