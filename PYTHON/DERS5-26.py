e5ef91844cf5f5eedb3c4190f0d580c747596e6a
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:23:32 +0300

    Create DERS5-26.py

---
+++ PYTHON/DERS5-26.py
@@ -0,0 +1,21 @@
+#100den küçük ikiz asalları bir liste olarak döndüren algo
+
+def isprime(n):
+    i = 3
+    if(n<2):
+        return(0)
+    if(n!=2 and n%2==0):
+        return(0)
+    while(i<=n**(1/2)):
+        if(n%i==0):
+            return(0)
+        i += 2
+    return(1)
+
+ikizasallar = []
+
+for n in range(3,101,2):
+    if(isprime(n)==1 and isprime(n+2)==1):
+        ikizasallar.append([n,n+2])
+
+print(ikizasallar)
