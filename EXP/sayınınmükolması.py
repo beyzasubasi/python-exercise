3e473319be9cee70f9788710748901c8005bd242
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:21:44 +0300

    Create sayınınmükolması.py

---
+++ EXP/sayınınmükolması.py
@@ -0,0 +1,22 @@
+#Kullanıcıdan aldığınız bir sayının mükemmel olup olmadığını bulmaya çalışın.
+#Bir sayının kendi hariç bölenlerinin toplamı kendine eşitse bu sayıya "mükemmel sayı" denir.
+#Örnek olarak, 6 mükemmel bir sayıdır. (1 + 2 + 3 = 6)
+
+
+sayı = int(input("Bir Sayı Giriniz:"))
+
+i=1
+toplam=0
+
+while(i<sayı):
+
+    if(sayı%i==0):
+        toplam += i
+    i+=1
+
+if(toplam==sayı):
+    print("Mükemmel sayıdır..")
+
+else:
+    print("Mükemmel sayı değildir..")
+
