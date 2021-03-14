e492c7db1bbc007e685cf15b4f45e6e221f15bab
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:21:19 +0300

    Create fonklaekok.py

---
+++ EXP/fonklaekok.py
@@ -0,0 +1,40 @@
+#Kullanıcıdan 2 tane sayı alarak bu sayıların en küçük ortak katlarını (EKOK) dönen bir tane fonksiyon yazın.
+# iki sayının en küçük ortak katı isteniyor örn 6 ile 8 in ekoku 24tür
+
+???????????????????????ANLAMADIM????????????????????????
+
+
+def ekokbul(sayı1, sayı2):
+    i = 2
+    ekok = 1
+
+    while True:
+
+        if (sayı1 % i == 0 and sayı2 % i == 0):
+            ekok *= i
+
+            sayı1 //= i
+            sayı2 //= i
+
+
+        elif (sayı1 % i == 0 and sayı2 % i != 0):
+            ekok *= i
+
+            sayı1 //= i
+
+
+        elif (sayı1 % i != 0 and sayı2 % i == 0):
+            ekok *= i
+
+            sayı2 //= i
+        else:
+            i += 1
+        if (sayı1 == 1 and sayı2 == 1):
+            break
+    return ekok
+
+
+sayı1 = int(input("Sayı-1:"))
+sayı2 = int(input("Sayı-2:"))
+
+print("Ekok:", ekok_bulma(sayı1, sayı2))
