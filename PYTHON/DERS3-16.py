5c41b177d944c09dfef99fb4dbef2904a90d957e
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:23:09 +0300

    Create DERS3-16.py

---
+++ PYTHON/DERS3-16.py
@@ -0,0 +1,11 @@
+#100den küçük bir sayının asal olup olmadığını bulan algoritma
+
+n = int(input("bir sayı girin: "))
+
+if(n%2==0 or n%3==0 or n%5==0 or n%7==0):
+
+    print("asal sayı değildir")
+
+else:
+
+    print("asal sayıdır")
