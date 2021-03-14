5201af7bb1f07a60be4623d3b127f4b0fb277e17
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:17:59 +0300

    Create 01exercise.py

---
+++ EXP/01exercise.py
@@ -0,0 +1,31 @@
+# Kullanıcıdan aldığınız 3 tane sayıyı çarparak ekrana yazdırın. Ekrana yazdırma işlemini format metoduyla yapmaya çalışın.
+
+a = int(input("Birinci Sayıyı Giriniz:"))
+print("{}".format(a))
+
+b = int(input("İkinci Sayıyı Giriniz:"))
+print("{}".format(b))
+
+c = int(input("Üçüncü Sayıyı Giriniz:"))
+print("{}".format(c))
+
+print("Bu sayıların çarpımı :",a*b*c)
+print("{}".format(a*b*c))
+
+
+#a = int(input("a:"))
+#b = int(input("b:"))
+#c = int(input("c:"))
+
+#çarpım = a * b * c
+
+#print("{} x {} x {} = {} dir".format(a,b,c,çarpım))
+
+#çıktı
+#a:1
+#b:3
+#c:10
+#1 x 3 x 10 = 30 dir
+
+
+
