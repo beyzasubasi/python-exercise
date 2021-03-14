96151cd92f8eb9391d3768a9aa098ef420676742
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:21:55 +0300

    Create DERS1-1.py

---
+++ PYTHON/DERS1-1.py
@@ -0,0 +1,8 @@
+#kullanıcıdan 2 sayı okuyup toplamını çıktı olarak veren algoritma
+
+sayi1 = int(input("sayi1 girin: "))
+sayi2 = int(input("sayi2 girin: "))
+
+toplam = sayi1 + sayi2
+
+print(toplam)
