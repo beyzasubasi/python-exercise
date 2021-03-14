06b0a42d74cc5d09a0d606ff11e54ec4659716f7
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:18:03 +0300

    Create 03exercise.py

---
+++ EXP/03exercise.py
@@ -0,0 +1,19 @@
+#Bir aracın kilometrede ne kadar yaktığı ve kaç kilometre yol yaptığı bilgilerini alın ve sürücünü toplam ne kadar ödemesini gerektiğini hesaplayın.
+
+a = float(input("Km de ne kadar yakıyorsunuz?"))
+b = int(input("Kaç km yol yaptınız"))
+
+ödeme = a*b
+print("Ödemeniz gereken tutar:{}tl".format(ödeme))
+
+
+
+
+#yakan_miktar = float(input("Kilometrede yakan miktar:"))
+
+#kilometre = int(input("Kaç km yol yaptınız:"))
+
+#print("Tutar: {} tl".format(yakan_miktar * kilometre))
+#Kilometrede yakan miktar:0.22
+#Kaç km yol yaptınız:430
+#Tutar: 94.6 tl
