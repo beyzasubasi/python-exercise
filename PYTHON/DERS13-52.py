009f56564fee437830350fca9bcfffff54399009
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:22:29 +0300

    Create DERS13-52.py

---
+++ PYTHON/DERS13-52.py
@@ -0,0 +1,14 @@
+#1 milyonun altındaki hangi asal sayı en uzun serinin(asal sayhıların) toplamı şeklinde yazılabilir
+
+
+from fonksiyon import allprimes
+from fonksiyon import isprime
+
+asal = allprimes(4000)
+
+for p in range(5,len(asal)):
+    for a in range(0,len(asal)-p):
+        if(isprime(sum(asal[a:a+p]))==1 and sum(asal[a:a+p])<1000000):
+            toplam = sum(asal[a:a+p])
+
+print(toplam)
