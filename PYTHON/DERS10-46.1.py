a5c8e1edeeddadfdcd89b56540d395110dd3086e
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:22:14 +0300

    Create DERS10-46.1.py

---
+++ PYTHON/DERS10-46.1.py
@@ -0,0 +1,16 @@
+#46 daki sorunun bir farklı çözümü
+
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+maks=[]
+for n in range(1,1000000):
+    liste=[]
+    while n!=1:
+        if n%2==0:
+            n=n/2
+        else:
+            n=3*n+1
+        liste.append(n)
+    maks.append(len(liste)+1)
+print(maks.index(max(maks))+1)
