78ef9d4a21136e6bc8d3d9fa7b658b9436bf1685
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:24:07 +0300

    Create DERS9-42.1.py

---
+++ PYTHON/DERS9-42.1.py
@@ -0,0 +1,22 @@
+#42.soruyu daha pratik olsun diye yarısına kadar değil karekökğne kadar olan bölenlerini yazdırıyorum
+
+def d(p):
+    toplam = 1
+    q = int(p**(1/2)//1)+1
+    for n in range(2, q):
+        if(p//n*n==p):
+            toplam += n  + p/n
+    return(toplam)
+
+gtoplam = 0
+
+for m in range(2,10000):
+    s = d(m)
+    if(m==s):
+        continue
+    if(s>10000):
+        continue
+    if(m==d(s)):
+        gtoplam += m
+
+print(gtoplam)
