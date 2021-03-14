c88da7abf258708280b81b42d563560654afd87b
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:18:05 +0300

    Create 04exercise.py

---
+++ EXP/04exercise.py
@@ -0,0 +1,26 @@
+#Kullanıcıdan ad,soyad ve numara bilgisini alarak bunları alt alta ekrana yazdırın.
+
+ad = input("Ad:")
+soyad = input("Soyad:")
+numara = input("Numara:")
+
+print("Ad: \nSoyad: \nNumara: ",ad\n ,soyad\n,numara)
+
+#FORMAT FONK KULLANMADAN NASIL YAZARIZ??????
+
+
+
+#ad = input("Ad:")
+#soyad = input("Soyad:")
+#numara = input("Numara:")
+#print("\nBilgiler...\n")
+#print("{}\n{}\n{}".format(ad,soyad,numara))
+#Ad:Mustafa Murat
+#Soyad:Coşkun
+#Numara:12345
+
+#Bilgiler...
+
+#Mustafa Murat
+#Coşkun
+#12345
