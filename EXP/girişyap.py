9ef8a5264502cb367970a4c143a33ebae9788330
Beyza Subaşı <beyza_subasi@outlook.com>
2021-03-16 01:31:24 +0300

    Update girişyap.py

--- EXP/girişyap.py
+++ EXP/girişyap.py
@@ -6,16 +6,16 @@ kullanıcışifrem = "123456789"
 
 
 KA = input("kullanıcı adı:")
-KŞ = input("kullanıcı şifresi:")
+KS = input("kullanıcı şifresi:")
 
 
-if(kullanıcıadım == KA and kullanıcışifrem != KŞ):
+if(kullanıcıadım == KA and kullanıcışifrem != KS):
     print("kullanıcı adınız doğru fakat şifreniz yanlış")
 
-elif(kullanıcıadım != KA and kullanıcışifrem == KŞ):
+elif(kullanıcıadım != KA and kullanıcışifrem == KS):
     print("kullanıcı adınız yanlış fakat şifreniz doğru")
 
-elif(kullanıcıadım != KA and kullanıcışifrem != KŞ):
+elif(kullanıcıadım != KA and kullanıcışifrem != KS):
     print("kullanıcı adınız ve şifreniz yanlış")
 else:
     print("Giriş başarılı ..")
